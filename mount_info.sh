d24f5ba559893b5189b1218e6eb65acf4ad273a5
unknown <wangjian@deepwise.com>
2019-01-15 21:03:12 +0800

    mount

---
+++ mount_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ftp 用户目录下挂载新空间
+#@reboot /bin/bash -c '/bin/mount --bind /opt/data3/ /data/ftp/personal/chenyang/data3'
+
+# ntfs
+#mount -t ntfs-3g /dev/disk_part_name /mnt/mount_path
